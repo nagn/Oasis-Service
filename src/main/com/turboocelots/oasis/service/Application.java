5fd8d47f238f5dee37eae4b8f0f6c09d3080898c
nagn <mlin4589@gmail.com>
2017-02-25 04:25:13 -0500

    Initial Commit

---
+++ src/main/com/turboocelots/oasis/service/Application.java
@@ -0,0 +1,15 @@
+package com.turboocelots.oasis.service;
+
+/**
+ * Created by mlin on 2/25/17.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
