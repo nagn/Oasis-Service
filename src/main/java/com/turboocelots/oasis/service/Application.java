9e6eb443832ccd83c0da5eede36bf5b03e2cadb7
nagn <mlin4589@gmail.com>
2017-02-25 19:36:56 -0500

    Now we have UserRoles and Roles Deleting users cascades a corresponding deletion from UserRoles

--- src/main/java/com/turboocelots/oasis/service/Application.java
+++ src/main/java/com/turboocelots/oasis/service/Application.java
@@ -33,12 +33,51 @@ public class Application implements CommandLineRunner {
         log.info("Creating tables");
 
         //TODO: eliminate table re-creation once we want data persistence
+        jdbcTemplate.execute("DROP TABLE IF EXISTS UserRoles");
+
         jdbcTemplate.execute("DROP TABLE IF EXISTS Users");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS Roles");
+
         jdbcTemplate.execute("CREATE TABLE Users(" +
-                "user_id SERIAL, name VARCHAR(255), password VARCHAR(255), " +
+                "user_id SERIAL PRIMARY KEY, name VARCHAR(255), password VARCHAR(255), " +
                 "email VARCHAR(255), title VARCHAR(255), address VARCHAR(255), " +
                 "phone_number VARCHAR(255), created_at TIMESTAMP, updated_at TIMESTAMP, " +
                 "banned BOOLEAN)");
+
+        // Roles. Currently populated with Administrator, Reporter, Manager, Worker
+        // Users can be promoted to several different types of roles
+        jdbcTemplate.execute("CREATE TABLE Roles(" +
+                "role_id SERIAL PRIMARY KEY, name VARCHAR(255), description TEXT)");
+
+        // TODO: Automate this process
+        jdbcTemplate.update(
+                "INSERT INTO Roles(name, description) VALUES(?, ?)",
+                "Administrator", "Special account for maintenance. Can delete accounts, " +
+                        "ban a user from submitting reports, and unblock a locked accounts. 4" +
+                        "Can also view the Security log.");
+
+        jdbcTemplate.update(
+                "INSERT INTO Roles(name, description) VALUES(?, ?)",
+                "User", "Basic account. Can submit a report on water availability and view available water sources.");
+
+        jdbcTemplate.update(
+                "INSERT INTO Roles(name, description) VALUES(?, ?)",
+                "Worker", "Can report on water purity levels.");
+
+
+        jdbcTemplate.update(
+                "INSERT INTO Roles(name, description) VALUES(?, ?)",
+                "Manager", "Can view historical reports and trends of water purity." +
+                        "Can also delete innaccurate individual reports.");
+
+        // Create UserRoles Table. This Table assigns permissions to a given user
+        // Note that a user may have several Roles
+
+        jdbcTemplate.execute("CREATE TABLE UserRoles(" +
+                "user_role_id SERIAL PRIMARY KEY," +
+                "user_id int4 REFERENCES Users(user_id) ON DELETE CASCADE," +
+                "role_id int4 REFERENCES Roles(role_id) ON DELETE CASCADE)");
 
         // Populate a bunch of users
         List<Object[]> names = Arrays.asList("John pass", "Jeff pass2", "Josh pass3", "Josh pass4").stream()
